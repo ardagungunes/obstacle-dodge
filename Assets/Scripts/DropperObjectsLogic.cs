2ae81b00014048cee6955703307431383e508d77
Arda Gün Güneş <ardagungunes@gmail.com>
2025-08-05 00:42:24 +0300

    Improved dropping mechanic

--- Assets/Scripts/DropperObjectsLogic.cs
+++ Assets/Scripts/DropperObjectsLogic.cs
@@ -4,11 +4,14 @@ public class DropperObjectsLogic : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     Rigidbody rb;
+    MeshRenderer meshRenderer;
     float dropTime = 3f;
     
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.enabled = false;
         rb.useGravity = false;
         
     }
@@ -20,6 +23,7 @@ public class DropperObjectsLogic : MonoBehaviour
         if(Time.time > dropTime)
         {
             rb.useGravity=true;
+            meshRenderer.enabled = true;
             dropTime = 0f;
         }
     }
